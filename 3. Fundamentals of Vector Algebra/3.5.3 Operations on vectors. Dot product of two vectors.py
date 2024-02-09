673d8c822a61f123a9d90fd0a3490212da132920
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2024-02-09 16:06:49 +0200

    3.5.3 Operations on vectors. Dot product of two vectors.py

--- 3. Fundamentals of Vector Algebra/3.5.3 Operations on vectors. Dot product of two vectors.py
+++ 3. Fundamentals of Vector Algebra/3.5.3 Operations on vectors. Dot product of two vectors.py
@@ -6,11 +6,13 @@ the product of the moduli of these vectors and the cosine of the angle between t
     α × β = |α| × |β| × cos(φ)
     
 The scalar product can be expressed in terms of vector coordinates:
-
+    α = (a_x, a_y, a_z),
+    β = (b_x, b_y), b_z).
+    
+    α × β = a_x × b_x + a_y × b_y + a_z × b_z.
     
 If we transpose one of the vectors, then the scalar product can be
 express through the operation of matrix multiplication:
-
 
 B = (b_x, b_y, b_z).
 α × β = (a_x a_y a_z) × B^T = a_x × b_x + a_y × b_y + a_z × b_z.
