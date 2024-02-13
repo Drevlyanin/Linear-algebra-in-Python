bc60db6cff244cebcd9846c6bd1b3505eb56d1de
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2024-02-13 19:03:05 +0200

    3.5.2 Operations on vectors. Multiplying a vector by a number.py

--- 3. Fundamentals of Vector Algebra/3.5.2 Operations on vectors. Multiplying a vector by a number.py
+++ 3. Fundamentals of Vector Algebra/3.5.2 Operations on vectors. Multiplying a vector by a number.py
@@ -1,1 +1,17 @@
+import numpy as np
+"""
+When multiplying a vector by a number,
+all vector components are multiplied by this number.
 
+    α = (a_x, a_y),
+    λ × α = (λ × a_x, λ × a_y),
+
+Let's multiply the vector α with coordinates (3, 2) by the number 2:
+    α = (3, 2),
+    2 × α = (2 × 3, 2 × 2) → (6, 4)
+"""
+
+a = np.array([3, 2])
+k = 2
+c = k * a
+print("Vector multiplied by a number:\n", c)
