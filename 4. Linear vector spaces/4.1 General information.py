bb6b81ab29b25bee540ee9aab81fc786bb9376c0
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2024-02-27 18:56:19 +0200

    4.1 General information.py

--- 4. Linear vector spaces/4.1 General information.py
+++ 4. Linear vector spaces/4.1 General information.py
@@ -1,15 +1,1 @@
 import numpy as np
-"""
-If we consider a vector as an algebraic object, then it can be represented as a row or column of numbers,
-those. as a matrix of size (1, n), (n, 1)^2.
-Within the framework of geometric interpretation, a directed segment of a straight line rises under the vector, characterized by the length
-and direction.
-
-Scalar quantity - characterized only by a numerical value.
-Vector quantity - characterized by numerical value and direction.
-
-A vector is denoted by one small Latin letter a, or two large Latin letters AB.
-a is the starting point of the vector.
-AB is the end of the vector.
-|a| - vector length (vector modulus).
-"""
