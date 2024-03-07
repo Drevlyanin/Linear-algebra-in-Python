09ff412327e09b76e61bc4ea436391cdf9faa190
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2024-03-07 22:26:52 +0200

    Update 2.2 Solving systems of linear equations. Cramer method.py

--- 2. Solving systems of linear equations/2.2 Solving systems of linear equations. Cramer method.py
+++ 2. Solving systems of linear equations/2.2 Solving systems of linear equations. Cramer method.py
@@ -4,7 +4,6 @@ import numpy as np
 Cramer's method is suitable for solving inhomogeneous systems of linear equations.
 N.B. The determinant of the main matrix of the system must not be different from zero!
 """
-
 A = np.matrix(' 3 -1 2; 1 4 -1; 2 3 1') # Main matrix of the system with known components of the left side.
 B = np.matrix('-4; 10; 8') # Matrix of elements of the right sides.
 print("Matrix with know components:\n", A)
@@ -24,6 +23,7 @@ print(Y_m)
 Z_m = np.matrix(A)
 Z_m[:, 2] = B
 print(Z_m)
+
 
 x = np.linalg.det(X_m) / A_det
 y = np.linalg.det(Y_m) / A_det
