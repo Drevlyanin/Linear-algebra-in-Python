89f9a4fbfd6bcbee1936535f975e6f70160ba658
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2024-04-03 16:41:15 +0300

    Add files via upload

---
+++ 6. Complex numbers/6.1 What is a complex number.py
@@ -0,0 +1,51 @@
+import numpy as np
+"""
+Natural numbers, they
+are indicated by a letter N and are used to count objects. It is believed that
+the smallest natural number is 1, the next is 2, etc.:
+    N = {1, 2, ..., ∞}.
+    
+If we add and multiply natural numbers, the result will be
+also natural numbers. But for subtraction this is not always feasible.
+
+In order to be able to use the “subtraction” operation, a set
+natural numbers must be expanded to the set of integers, it
+denoted by Z it includes natural numbers and their negative inverses
+numbers and zero:
+    Z = {-∞, ..., -2, -1, 0, 1, 2, ..., ∞}.
+    
+Adding, subtracting and multiplying integers produces a whole number. But for the operation
+“division” is not it.
+
+Let's expand the set of numbers, this time our new set will be
+contain the entire set of integers and all the fractions that can be
+presented as a ratio of numerator and denominator, the latter of which is not
+equal to zero. Such a set is called the set of rational numbers and
+denoted by Q:
+    a/b ∈ Q if a ∈ Z, b ∈ Z, b ≠ 0ю
+    
+In addition to simple arithmetic operations (addition, subtraction, multiplication and
+division) when working with numbers, you often have to use operations: raising to
+degree and root extraction. Again, if we take a suitable rational
+number and take the square root of it:
+    √4/9 = 2/3
+we get a rational number.
+
+By analogy with the above, you can already guess that there are such
+rational numbers whose square root is not rational
+number, as an example we can use the number 2:
+    √2 = 1.41421356237
+    
+Such numbers are called irrational. Sets of rational and
+irrational numbers make up the set of real numbers, it
+denoted as R.
+
+But the operation of extracting the square root is quite insidious; there are such
+real numbers whose square root cannot be represented
+another real number. For example, the square root of (-1) is not
+real number. Before becoming familiar with complex numbers, as a rule, even
+they say that such a root does not exist.
+
+The set of complex numbers C is a set of pairs of values (a, b),
+ where a is called the real part, and b is the imaginary part of the complex number.
+"""
